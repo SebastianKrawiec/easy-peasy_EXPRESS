325c87fc8f193d9948c78337d0d5b5c0f24edb68
Sebastian Krawiec <sebastian.arielk@gmail.com>
2020-11-21 17:36:27 -0300

    agregando modificaciones

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { userInfo } = require("os");
 const app = express();
 
 app.listen(3000, ()=> console.log("servidor corriendo en puerto 3000"));
 
 const rutaEasy = require("./routes/easy-peasy");
+const rutaMovies = require("./routes/movies");
+/* const rutaFaqs = require("./routes/faqs") */
+/* const rutaMovies = require("./") */
 
 app.use("/", rutaEasy);
+app.use("/peliculas", rutaMovies);
+/* app.use("/faqs", rutaFaqs); */
